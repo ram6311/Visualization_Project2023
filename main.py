dd8018839ca7dabad9101d79c262fa708d69ab19
Ram Manor <84604391+ram6311@users.noreply.github.com>
2023-06-17 18:55:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import streamlit as st
+import pandas as pd
+
+df = pd.read_csv('heart_data.csv')
+first_row = df.head(1)
 
 def main():
     st.write("Hello, World!")
+    st.write(first_row)
+    
 
 if __name__ == "__main__":
     main()
