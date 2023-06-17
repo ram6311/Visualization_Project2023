49475fa6abc0b4adb507c468a0cfe8d50ad4542e
Ram Manor <84604391+ram6311@users.noreply.github.com>
2023-06-17 21:22:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ import plotly.express as px
 
 # Read the dataset
 df = pd.read_csv('heart_2020_cleaned.csv')
-print(df.head())
+
 def main():
-    # Create the Streamlit app
-    st.title("Interactive Plot")
+    # # Create the Streamlit app
+    # st.title("Interactive Plot")
     
-    # Select the variable for x-axis
-    x_variable = st.selectbox("Select X variable:", df.columns)
+    # # Select the variable for x-axis
+    # x_variable = st.selectbox("Select X variable:", df.columns)
 
-    # Plotting the data using Plotly Express
-    fig = px.histogram(df, x=x_variable)
-    st.plotly_chart(fig)
-
+    # # Plotting the data using Plotly Express
+    # fig = px.histogram(df, x=x_variable)
+    # st.plotly_chart(fig)
+    st.write(df.head())
 
 if __name__ == "__main__":
     main()
