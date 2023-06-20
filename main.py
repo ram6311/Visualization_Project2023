eb1e6312ff09a5951aa57dfb429d5e1a3f95671f
adarharari <84996030+adarharari@users.noreply.github.com>
2023-06-20 19:46:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,47 @@ def main():
     # st.write(df.head())
         
     # Group by 'Category' and calculate the sum of 'Value'
-    grouped_df = df.groupby(['AgeCategory',  'Sex', 'Race','HeartDisease' ]).sum()
+   import plotly.graph_objects as go
     
-    print(grouped_df)
+    fig = go.Figure()
+    
+    fig.update_layout(
+        template="simple_white",
+        xaxis=dict(title_text="AgeCategory"),
+        yaxis=dict(title_text="Count"),
+        barmode="group",
+    )
+    
+    colors = ["#2A66DE", "#FFC32B", "#FF5733", "#8B008B", "#00FF00", "#FFD700"]
+    
+    # Filter the DataFrame for female with heart disease
+    female_hd_df = grouped_df[(grouped_df['Sex'] == 'Female') & (grouped_df['HeartDisease'] == 'Yes')]
+    fig.add_trace(
+        go.Bar(x=female_hd_df['AgeCategory'], y=female_hd_df['Count'], name='Female with Heart Disease', marker_color=colors[0])
+    )
+    
+    # Filter the DataFrame for female without heart disease
+    female_no_hd_df = grouped_df[(grouped_df['Sex'] == 'Female') & (grouped_df['HeartDisease'] == 'No')]
+    fig.add_trace(
+        go.Bar(x=female_no_hd_df['AgeCategory'], y=female_no_hd_df['Count'], name='Female without Heart Disease', marker_color=colors[1])
+    )
+    
+    # Filter the DataFrame for male with heart disease
+    male_hd_df = grouped_df[(grouped_df['Sex'] == 'Male') & (grouped_df['HeartDisease'] == 'Yes')]
+    fig.add_trace(
+        go.Bar(x=male_hd_df['AgeCategory'], y=male_hd_df['Count'], name='Male with Heart Disease', marker_color=colors[2])
+    )
+    
+    # Filter the DataFrame for male without heart disease
+    male_no_hd_df = grouped_df[(grouped_df['Sex'] == 'Male') & (grouped_df['HeartDisease'] == 'No')]
+    fig.add_trace(
+        go.Bar(x=male_no_hd_df['AgeCategory'], y=male_no_hd_df['Count'], name='Male without Heart Disease', marker_color=colors[3])
+    )
+    
+    
+    st.plotly_chart(fig)
+
+
     
 
 if __name__ == "__main__":
